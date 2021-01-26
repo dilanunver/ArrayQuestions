b4517f15581c6cd7e8c6a53868abdbb181d1a7e7
Dilan <dunver33@gmail.com>
2021-01-26 14:23:22 +0300

    first and last element of array

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-//Write a JavaScript function to get the last element of an array. Passing a parameter 'n' will return the last 'n' elements of the array. 
+//Write a JavaScript function to get the last element of an array. 
+//Passing a parameter 'n' will return the last 'n' elements of the array. 
 
 const button = document.querySelector('button');
 const element = document.querySelector('h1');
 
-var newArray = function(array,n){
-  var gettingArray = [];
-
-  if(array.length > 0 && !n ){ 
+var lastElement = function(array,n){
+  var takingArray = [];
+  if(array.length > 0 && !n){
     return array[array.length-1]
+
   }
-  if(array.length > 0 && n ) {
-      for(let i = 0; i<array.length; i++){
-      if(n>i) { 
-        gettingArray.push(array[i]) 
-      } 
+  if(array.length > 0 && n ){
+    var j=0;
+    for(let i = array.length-1; i>=0; i--){
+      if(n>j){
+        takingArray.push(array[i]) 
+      }
+      j++
     }
-  } else { 
+  }
+    else { 
     return 'empty value'
   }
-  console.log(gettingArray)
-};
+  console.log(takingArray)
+  return takingArray;
+}
 button.addEventListener('click', ()=>{
-  element.innerText = newArray([ 7, 9, 0, -2], 3)
+  element.innerText = lastElement([ 7, 9, 0, -2],3)
 });
+
 
 // var result = function(lastArray, n){
 //   if(lastArray == null) return 'undefined array';
@@ -32,4 +38,3 @@ button.addEventListener('click', ()=>{
 // button.addEventListener('click', ()=>{
 //   element.innerText = result([ 7, 9, 0, -2],6)
 // })
-
