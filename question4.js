ac491d00e9458fa470153b7e5df21a261b79187c
Dilan <dunver33@gmail.com>
2021-01-28 18:45:16 +0300

    question 4:finding a most frequent item of an array

---
+++ question4.js
@@ -0,0 +1,18 @@
+function mostFrequent (arr){
+  let mf = arr[0];
+  let maxCount = 0;
+  for(let i = 0; i<arr.length; i++){
+    let count = 0;
+    for(let j = i+1; j<arr.length; j++){
+      if(arr[i] == arr[j]){
+        count++;
+      }
+    }
+    if(maxCount < count){
+      maxCount = count;
+      mf = arr[i];
+    }
+  }
+  return mf;
+}
+console.log(mostFrequent([3,3,3,5,5,5,5,5,7]));
