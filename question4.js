26bc40fbfc2fcfc83411397244d5ea96b8a2a07a
Dilan <dunver33@gmail.com>
2021-02-01 11:41:09 +0300

    Question 5::swap the case

--- question4.js
+++ question4.js
@@ -1,7 +1,10 @@
+//Write a JavaScript program to find the most frequent item of an array.
+
+
 function mostFrequent (arr){
   let mf = arr[0];
   let maxCount = 0;
-  for(let i = 0; i<arr.length; i++){
+  for(let i = 0 ; i<arr.length; i++){
     let count = 0;
     for(let j = i+1; j<arr.length; j++){
       if(arr[i] == arr[j]){
---
+++ question5.js
@@ -0,0 +1,20 @@
+// Swap the case of each character of a string, upper case to lower and vice versa
+
+var str = 'The Quick Brown Fox';
+var Upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var Lower = 'abcdefghijklmnopqrstuvwxyz';
+var result = [];
+
+for ( let x = 0; x<str.length; x++){
+  if(Upper.indexOf(str[x]) !== -1){
+    result.push(str[x].toLowerCase());
+  }
+  else if(Lower.indexOf(str[x]) !== -1)
+    {
+      result.push(str[x].toUpperCase());
+    } else {
+    result.push(str[x]);
+  }
+}
+console.log(result.join(''));
+
