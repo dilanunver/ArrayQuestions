d45028a6744d1d636db319cbb39577229924acda
Dilan <dunver33@gmail.com>
2021-02-02 11:15:19 +0300

    question 6 :: another solution of the question

--- question6.js
+++ question6.js
@@ -1,11 +1,18 @@
 // Write a JavaScript program which prints the elements of the following array. 
 
+// var array = [[1,2,1,24], [8,11,9,4],[7,0,7,27], [7,4,28,14], [3,10,26,73]];
+// for(let i = 0; i<array.length; i++){
+//   console.log('"' + 'row '+ i + '"')
+//   for(let j = 0; j<array[i].length; j++){
+//     console.log('"' + array[i][j] + '"')
+// }
+// }
 
-var array = [[1,2,1,24], [8,11,9,4],[7,0,7,27], [7,4,28,14], [3,10,26,73]];
-for(let i = 0; i<array.length; i++){
-  console.log('"' + 'row '+ i + '"')
-  for(let j = 0; j<array[i].length; j++){
-    console.log('"' + array[i][j] + '"')
-}
-  
+var a = [[1,2,1,24], [8,11,9,4], [7,0,7,27], [7,4,28,14], [3,10,26,73]];
+
+for(var i in a){
+  console.log('row ' + i);
+  for(var j in a[i]){
+    console.log('' + a[i][j])
+  }
 }
