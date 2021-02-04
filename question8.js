6ff41d2180587618ed13d6a03f8389693cd5e69d
Dilan <dunver33@gmail.com>
2021-02-04 13:34:36 +0300

    question8::another solution

--- question8.js
+++ question8.js
@@ -1,25 +1,41 @@
 // Write a JavaScript program to find duplicate values in a JavaScript array
 
-function findDuplicate(array1){
-  var object = {};
-  var result = [];
+const findDups = (arr) => {
+  const dups = [];
+  const compare = [];
 
-  array1.forEach(element => {
-    if(!object[element]){
-      object[element] = 1; 
-      
+  for(let num of arr){
+    if(!compare.includes(num)){
+      compare.push(num)
     } else {
-      object[element] += 1; 
-    } 
-  
-  });
-  for(var prop in object){
-    console.log(prop)
-    if(object[prop] >= 2){
-      console.log(object[prop])
-      result.push(prop)
+      dups.push(num)
     }
   }
-  return result;
+  return dups
 }
-console.log(findDuplicate(['a','a','a','b','c','d','e','e','e','e']))
+const arr = [4,3,2,7,8,2,3,1]
+console.log(findDups(arr))
+
+// function findDuplicate(array1){
+//   var object = {};
+//   var result = [];
+
+//   array1.forEach(element => {
+//     if(!object[element]){
+//       object[element] = 1; 
+      
+//     } else {
+//       object[element] += 1; 
+//     } 
+  
+//   });
+//   for(var prop in object){
+//     console.log(prop)
+//     if(object[prop] >= 2){
+//       console.log(object[prop])
+//       result.push(prop)
+//     }
+//   }
+//   return result;
+// }
+// console.log(findDuplicate(['a','a','a','b','c','d','e','e','e','e']))
