7e081774cf77c5fa23a685050b608387c06695a7
Dilan <dunver33@gmail.com>
2021-02-11 14:05:04 +0300

    largest numbers of four arrays

--- question15.js
+++ question15.js
@@ -9,23 +9,23 @@
 // console.log(repeatString('abc', 3));
 
 
-// function repeatString(str,num){
-//   var final = '';
-//   if(num<0) return '';
-
-//   for(var i = 0; i<num; i++){
-//     final += str
-//   }
-//   return final
-// }
-// repeatString('abc', 3)
-// console.log(repeatString('abc', 3))
-
 function repeatString(str,num){
+  var final = '';
   if(num<0) return '';
-  if(num === 1) return str;
 
-  return str + repeatString(str, num-1)
+  for(var i = 0; i<num; i++){
+    final += str
+  }
+  return final
 }
-repeatString('abc', 8)
-console.log(repeatString('abc', 8))
+repeatString('abc', 3)
+console.log(repeatString('abc', 3))
+
+// function repeatString(str,num){
+//   if(num<0) return '';
+//   if(num === 1) return str;
+
+//   return str + repeatString(str, num-1)
+// }
+// repeatString('abc', 8)
+// console.log(repeatString('abc', 8))
