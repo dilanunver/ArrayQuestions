cec3398bc61e5c7fa81503bc2eed41bb668b2828
Dilan <dunver33@gmail.com>
2021-02-12 10:20:33 +0300

    falsy bouncer

---
+++ question18.js
@@ -0,0 +1,24 @@
+// falsy bouncer
+
+// function bouncer(arr){
+//   var truthies = [];
+//   for(var num of arr){
+//     if(num) truthies.push(num)
+//   }
+//   return truthies
+
+// }
+
+// bouncer([7, 'ate', '', 9, 'hey', undefined])
+// console.log(bouncer([7, 'ate', '', 9, 'hey', undefined]))
+
+
+function bouncer(arr){
+  return arr.filter(function(num){
+    return num
+  })
+
+}
+
+bouncer([7, 'ate', '', 9, 'hey', undefined])
+console.log(bouncer([7, 'ate', '', 9, 'hey', undefined]))
