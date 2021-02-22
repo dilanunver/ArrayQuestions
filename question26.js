5caa9f68dcc629ed567960090451148e00156414
Dilan <dunver33@gmail.com>
2021-02-22 12:05:02 +0300

    truncate String

---
+++ question26.js
@@ -0,0 +1,5 @@
+function truncateString(str, num){
+  if(num>str.length) return str;
+    return str.slice(0, num) + '...'
+}
+console.log(truncateString('A-tisket a-tasket A green and yellow basket', 8))
